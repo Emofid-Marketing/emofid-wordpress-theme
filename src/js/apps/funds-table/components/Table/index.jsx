c153a5303ca42331b6237e8ae8eecd81cb6b7e95
Majid Ghafoorzade <m_ghafoorzadeh@namava.local>
2021-12-08 12:05:46 +0330

    update funds page table

--- src/js/apps/funds-table/components/Table/index.jsx
+++ src/js/apps/funds-table/components/Table/index.jsx
@@ -1,11 +1,43 @@
 import classNames from "classnames";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import fundsTableColumns from "../../data/fundsTableColumns";
 import TableRow from "../TableRow/index.jsx";
 import funds from "../../data/funds";
 import styles from "./styles.module.scss";
+import numeral from "numeral";
 
 function Table() {
+  const [fundsData, setFundsData] = useState(false);
+
+  useEffect(async () => {
+    let response = await fetch(
+      "https://fundsapi.emofid.com/api/Investment/Returns"
+    );
+    if (response.ok) {
+      let data = [];
+      let jsonData = await response.json();
+      jsonData.forEach((fund) => {
+        if (!fund.staticInfo) return;
+        // console.log(fund.staticInfo.enName);
+        let thisFund = {
+          iconName: fund.staticInfo.enName.toLowerCase(),
+          title: fund.title,
+          type: fund.staticInfo.fundType,
+          year: fund.staticInfo.startDateFa.split("-")[0],
+          totalFund: numeral(Math.floor(fund.aum / 1000000000)).format("0,0"),
+          investers: numeral(fund.currentInvestorsNumber).format("0,0"),
+          performance: fund.return1Y,
+          tradeLink: "#$",
+          fundLink: "#%",
+        };
+        data.push(thisFund);
+      });
+      setFundsData(data);
+    }
+  }, []);
+
+  if (!fundsData) return <div>Loading</div>;
+
   return (
     <div
       className={classNames(
@@ -26,7 +58,7 @@ function Table() {
           })}
         </div>
         <div className={styles.body}>
-          {funds.map((fund, index) => {
+          {fundsData.map((fund, index) => {
             return <TableRow key={index} fund={fund} />;
           })}
         </div>
