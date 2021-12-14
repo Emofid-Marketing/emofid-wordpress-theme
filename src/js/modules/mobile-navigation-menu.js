c78d71378a00f0c68076387238a4c48484f020bd
Majid Ghafoorzade <m_ghafoorzadeh@namava.local>
2021-12-14 19:30:09 +0330

    update mobile menu

--- src/js/modules/mobile-navigation-menu.js
+++ src/js/modules/mobile-navigation-menu.js
@@ -4,9 +4,12 @@ $(document).ready(function () {
 
   $('.mobile-nav-btn').click(function () {
     $('.Navigation').toggleClass('active');
+    $('.nav-wrapper').toggleClass('active');
+    $('body').toggleClass('no-scroll');
   });
 
   $('.Navigation > ul > li').click(function (e) {
+    console.log("actived");
     $(this).toggleClass('active');
   });
 
--- src/js/modules/stiky-mobile-header.js
+++ src/js/modules/stiky-mobile-header.js
@@ -3,11 +3,32 @@ window.onscroll = function () {
 };
 
 var header = document.getElementById("header");
-var sticky = 68;
+var sticky = 150;
+var lastScrollTop = 0;
+
 function stickyHeader() {
+
   if (window.pageYOffset >= sticky) {
-    header.classList.add("sticked");
+
+    var st = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (st > lastScrollTop) {
+
+      if (header.classList.contains("sticked")) {
+        header.classList.add("unsticked");
+        header.classList.remove("sticked");
+      }
+
+    } else {
+      header.classList.remove("unsticked");
+      header.classList.add("sticked");
+    }
+
+    lastScrollTop = st <= 0 ? 0 : st;
+
   } else {
     header.classList.remove("sticked");
   }
+
+
 }
