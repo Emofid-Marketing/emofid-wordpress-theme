c690fa605322087de680029b9c5c602269951ff0
Majid Ghafoorzade <m_ghafoorzadeh@namava.local>
2022-01-12 21:05:11 +0330

    Task EM-202: Develope News Page

--- index.php
+++ index.php
@@ -5,7 +5,9 @@ require_once( __DIR__ . "/data/platforms-boxes.php" );
 require_once( __DIR__ . "/data/partners.php" );
 
 $context = Timber::context();
-$context['posts'] = Timber::get_posts();
+$context['posts'] = Timber::get_posts([
+    'posts_per_page' => 2,
+]);
 $context['advantages'] = $advantages;
 $context['boxes'] = $boxes;
 $context['partners'] = $partners;
---
+++ pages/news.php
@@ -0,0 +1,51 @@
+<?php
+
+/* Template Name: News Page */
+
+$context = Timber::context();
+
+
+/* Announcements */
+$announcements = array(
+    'post_type' => 'announcement',
+    'posts_per_page' => 1,
+);
+
+$context['announcements'] = Timber::get_posts( $announcements );
+
+
+/* Slider Posts */
+$slider_posts = array(
+    'post_type' => 'post',
+    'posts_per_page' => 5,
+);
+
+$context['slider_posts'] = Timber::get_posts( $slider_posts );
+
+
+/* Posts */
+$posts = array(
+    'post_type' => 'post',
+    'posts_per_page' => 3,
+);
+
+$context['posts'] = Timber::get_posts( $posts );
+
+
+/* Multimedia Posts */
+$multimedia_posts = array(
+    'post_type' => 'post',
+    'posts_per_page' => 2,
+);
+
+$context['multimedia_posts'] = Timber::get_posts( $multimedia_posts );
+
+
+/* Tags Cloud */
+$context['tags_cloud'] = wp_tag_cloud([
+    'echo' => 0,
+    'number' => 8
+]);
+
+
+Timber::render('pages/news.twig', $context);
