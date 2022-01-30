dd2fed482937bfd7e3dbfbfc0e5fa619fb894ca5
Majid Ghafoorzade <m_ghafoorzadeh@namava.local>
2022-01-30 17:59:44 +0330

    Resolve post views meta

--- single.php
+++ single.php
@@ -3,6 +3,7 @@
 $context = Timber::context();
 
 $context['post'] = new Timber\Post();
+$context['post_views'] = pvc_get_post_views(get_the_ID());
 $context['related_posts'] = getRelatedPostsByTag();
 
 Timber::render('pages/singles/post.twig', $context);
