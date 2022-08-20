4236fe1a50f9f06b8a80b8222e69fdfdc7bdb95a
Majid Ghafoorzade <m_ghafoorzadeh@namava.local>
2022-08-20 16:12:24 +0430

    Enable API CORS

--- config/security.php
+++ config/security.php
@@ -5,8 +5,18 @@ function modify_request($request, $isArray = false) {
     return $data;
 }
 
+
 // disable generator meta
 remove_action('wp_head', 'wp_generator');
 
+
 // disable XML-RPC
 add_filter('xmlrpc_enabled', '__return_false');
+
+
+// disable API CORS headers( Because Server handles it )
+add_action( 'rest_api_init', function() {
+
+	remove_filter( 'rest_pre_serve_request', 'rest_send_cors_headers' );
+
+}, 15 );
