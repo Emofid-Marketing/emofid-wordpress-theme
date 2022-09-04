398c4ebd6be46d30f47ce882ac24a645bb200ffb
Majid Ghafoorzade <m_ghafoorzadeh@namava.local>
2022-09-04 17:15:22 +0430

    add thumbnail to posts api

--- config/apis/posts.php
+++ config/apis/posts.php
@@ -11,6 +11,7 @@ register_rest_field(
 function get_post_data() {
     return [
         'post_views' => pvc_get_post_views(get_the_ID()),
-        'shamsi_date' => get_the_date()
+        'shamsi_date' => get_the_date(),
+        'thumbnail' => get_the_post_thumbnail_url()
     ];
 }
