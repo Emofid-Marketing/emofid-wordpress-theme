4e4c97ba5f1580d5a65052ad5e8b4f8b4e98d2a2
Majid Ghafoorzade <m_ghafoorzadeh@namava.local>
2022-12-20 20:32:46 +0330

    add hcaptcha to investment-advice API

--- config/apis/investment-advice.php
+++ config/apis/investment-advice.php
@@ -1,5 +1,7 @@
 <?php
 
+// print_r( $_SESSION );
+
 add_action( 'rest_api_init', function () {
     register_rest_route( 'investment-advice/v1', '/store', array(
       'methods' => 'POST',
@@ -16,6 +18,14 @@ function investment_advice_store_mobile( WP_REST_Request $request ) {
     $body = json_decode($request->get_body());
     $mobile = convertPersianCharsToEnglish($body->mobile);
     $location = $body->location || "";
+
+
+    // chech captcha
+    if( !validateCaptcha( $body->captcha ) ) return [
+        'status' => false,
+        'error' => 'کد تصویری وارد شده اشتباه است'
+    ];
+    
 
     // validate mobile number
     if( !validateMobileNumber($mobile) ) {
@@ -58,6 +68,22 @@ function investment_advice_store_mobile( WP_REST_Request $request ) {
     ];
 }
 
+
+function validateCaptcha( $token ) {
+    $data = array(
+        'secret' => "0x0000000000000000000000000000000000000000",
+        'response' => $token
+    );
+
+    $verify = curl_init();
+    curl_setopt($verify, CURLOPT_URL, "https://hcaptcha.com/siteverify");
+    curl_setopt($verify, CURLOPT_POST, true);
+    curl_setopt($verify, CURLOPT_POSTFIELDS, http_build_query($data));
+    curl_setopt($verify, CURLOPT_RETURNTRANSFER, true);
+    $response = json_decode(curl_exec($verify));
+
+    return $response->success;
+}
 
 function thisIPIsSpammer( $mobile ) {
 
