dede56d9a3d3b0941619669ecfea5571022c418e
Majid Ghafoorzade <m_ghafoorzadeh@namava.local>
2023-01-30 21:59:45 +0330

    replace hcaptcha with arcaptcha

--- config/utils/validateCaptcha.php
+++ config/utils/validateCaptcha.php
@@ -2,12 +2,12 @@
 
 function validateCaptcha( $token ) {
     $data = array(
-        'secret' => "0x033Ca3FBf903cBD12E6c9fd172309216502fb952",
+        'secret' => "49r043yqftliw5dub5p7",
         'response' => $token
     );
 
     $verify = curl_init();
-    curl_setopt($verify, CURLOPT_URL, "https://hcaptcha.com/siteverify");
+    curl_setopt($verify, CURLOPT_URL, "https://arcaptcha.co/3/siteverify");
     curl_setopt($verify, CURLOPT_POST, true);
     curl_setopt($verify, CURLOPT_POSTFIELDS, http_build_query($data));
     curl_setopt($verify, CURLOPT_RETURNTRANSFER, true);
